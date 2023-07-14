ef35e91ba8e74484c3c8c572e939d42fcd6640c8
aleixfortm <aleix.fort45@gmail.com>
2023-07-14 20:50:08 +0200

    additions

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-)
-
-type Response struct {
-	Message string `json:"message"`
-}
+import "go-wol/cmd"
 
 func main() {
-	http.HandleFunc("/ping", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	response := Response{Message: "pong!"}
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonResponse)
+	cmd.Execute()
 }
