f477ec05dd8a0eca77daef1c511d7d8b81436ba6
aleixfortm <aleix.fort45@gmail.com>
2023-07-15 21:58:48 +0200

    added config file

--- cmd/config.go
+++ cmd/config.go
@@ -4,32 +4,85 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/spf13/cobra"
 )
 
+type Config struct {
+	Port int `json:"port"`
+}
+
 // configCmd represents the config command
-var portValue string
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Show and tweak default app settings",
 	Long:  `Show and tweak default application settings such as broadcast IP address and UDP port.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Port:", portValue)
+		// Read the current configuration from the file
+		config, err := readConfigFromFile()
+		if err != nil {
+			fmt.Println("Failed to read config file:", err)
+			return
+		}
+
+		// Check if the --port flag is provided
+		portFlag, _ := cmd.Flags().GetInt("port")
+		if portFlag != 0 {
+			// Update the port value in the configuration
+			config.Port = portFlag
+
+			// Save the updated configuration to the file
+			err = saveConfigToFile(config)
+			if err != nil {
+				fmt.Println("Failed to save config file:", err)
+				return
+			}
+
+			fmt.Println("Configuration updated successfully!")
+		} else {
+			// Display the current configuration
+			fmt.Println("Current Configuration:")
+			fmt.Println("Port:", config.Port)
+		}
 	},
+}
+
+func readConfigFromFile() (*Config, error) {
+	filePath := "config.json"
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = json.Unmarshal(fileData, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func saveConfigToFile(config *Config) error {
+	filePath := "config.json"
+	fileData, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filePath, fileData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(configCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	rootCmd.Flags().StringVar(&portValue, "port", "p", "Set new default UDP port, recommended 7(Default) or 9")
+	configCmd.Flags().IntP("port", "p", 0, "Set new default UDP port, recommended 7(Default) or 9")
 }
