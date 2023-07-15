1a74d6a45454e6f01687d557daf6b01f7ab9244a
aleixfortm <aleix.fort45@gmail.com>
2023-07-15 18:51:36 +0200

    added remove command

--- cmd/remove.go
+++ cmd/remove.go
@@ -4,18 +4,70 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/spf13/cobra"
 )
 
 // removeCmd represents the remove command
 var removeCmd = &cobra.Command{
-	Use:   "rm",
-	Short: "Remove saved device",
-	Long:  `Remove a saved device by calling this command, in addition to the name of the device that you would like to delete`,
+	Use:     "remove",
+	Short:   "Remove saved device",
+	Long:    `Remove a saved device by calling this command, with the name of the device as argument. Use command "list" to see list of saved devices.`,
+	Args:    cobra.ExactArgs(1),
+	Example: `  woled remove PC-2`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("remove called")
+		deviceToRemove := args[0]
+
+		// Create new type to save JSON data to config file
+		type Device struct {
+			Name       string `json:"name"`
+			MACAddress string `json:"macAddress"`
+		}
+
+		// Read existing JSON file
+		filePath := "config.json"
+		configData, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			fmt.Println("Failed to read JSON file:", err)
+			return
+		}
+
+		// Unmarshal existing JSON data into slice of type Device
+		var deviceList []Device
+		err = json.Unmarshal(configData, &deviceList)
+		if err != nil {
+			fmt.Println("Failed to Unmarshall JSON data:", err)
+		}
+
+		// Find the index to remove
+		for i, d := range deviceList {
+			if d.Name == deviceToRemove {
+				deviceList = append(deviceList[:i], deviceList[i+1:]...)
+
+				// Convert device data to JSON
+				DeviceList, err := json.MarshalIndent(deviceList, "", "    ")
+				if err != nil {
+					fmt.Println("Failed to convert data to JSON:", err)
+					return
+				}
+
+				// Write JSON data to the configuration file
+				err = ioutil.WriteFile("config.json", DeviceList, 0644)
+				if err != nil {
+					fmt.Println("Failed to write configuration file:", err)
+					return
+				}
+
+				fmt.Println(deviceToRemove, "has been successfully deleted.")
+
+				return
+			}
+		}
+
+		fmt.Println(deviceToRemove, "was not found. Run command 'list' to check existing devices.")
 	},
 }
 
