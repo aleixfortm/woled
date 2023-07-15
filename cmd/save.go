afa8c833be0657f70feea2a32eb0d2ec3e36cf40
aleixfortm <aleix.fort45@gmail.com>
2023-07-15 12:07:01 +0200

    added new type struct

--- cmd/save.go
+++ cmd/save.go
@@ -15,16 +15,26 @@ var getCmd = &cobra.Command{
 	Long:  `Save your device to a local config file by specifying a name and the MAC address of the device.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
+		// Create new type to save JSON data to config file
+		type Device struct {
+			Name       string `json:"name"`
+			MACAddress string `json:"macAddress"`
+		}
+
+		// Check if two arguments have been given in the command, else cancel
 		if len(args) != 2 {
 			fmt.Println("Provide a name and a MAC address and try again")
 			return
 		}
 
-		name := args[0]
-		macAddress := args[1]
+		// Assign given arguments to Device type
+		device := Device{
+			Name:       args[0],
+			MACAddress: args[1],
+		}
 
-		fmt.Println("Name:", name)
-		fmt.Println("MAC:", macAddress)
+		fmt.Println("Name:", device.Name)
+		fmt.Println("MAC:", device.MACAddress)
 	},
 }
 
