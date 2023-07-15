aa5763312d344227424a3ed6e4b1a48c64440ca1
aleixfortm <aleix.fort45@gmail.com>
2023-07-15 21:05:02 +0200

    refactored variables

--- cmd/root.go
+++ cmd/root.go
@@ -11,9 +11,6 @@ var rootCmd = &cobra.Command{
 	Use:   "woled",
 	Short: "An over-engineered CLI WOL tool",
 	Long:  `An over-engineered CLI Wake-on-LAN tool`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -26,12 +23,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-wol.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 }
