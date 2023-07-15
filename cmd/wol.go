63a1146b958c92327f48d18d1502d8170c3b0d7b
aleixfortm <aleix.fort45@gmail.com>
2023-07-15 20:35:06 +0200

    added functionality

--- cmd/wol.go
+++ cmd/wol.go
@@ -90,15 +90,7 @@ var wolCmd = &cobra.Command{
 }
 
 func init() {
+	rootCmd.Flags().BoolP("new", "n", false, "Use the --new or -n flag to change the functionality")
 	rootCmd.AddCommand(wolCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// wolCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// wolCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
